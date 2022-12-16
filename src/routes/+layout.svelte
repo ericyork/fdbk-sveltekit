a4f83aefc0c90a502e8d3eedaeebbfbb7b7f0fa8
ericyork <eyork@clarkson.edu>
2022-12-16 13:24:17 -0500

    intermediate push

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <nav>
-	<a href="/">home</a>
-	<a href="/about">about</a>
-  <a href="/blog">blog</a>
+	<a href="/">Home</a>
+	<a href="/about">About</a>
+  <a href="/blog">Blog</a>
 </nav>
 
 <slot />
