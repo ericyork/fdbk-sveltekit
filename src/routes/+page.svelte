e73aff968c667afccc9482dba6a1afb4b1453d77
ericyork <eyork@clarkson.edu>
2022-12-16 12:13:08 -0500

    enables full routing with slugs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,2 @@
-<nav>
-	<a href="/">home</a>
-	<a href="/about">about</a>
-</nav>
 <h1>Welcome to SvelteKit</h1>
 <p>Working through the new <a href="https://learn.svelte.dev/tutorial">sveltekit tutorial</a>.</p>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,2 @@
-<nav>
-	<a href="/">home</a>
-	<a href="/about">about</a>
-</nav>
 <h1>About</h1>
 <p>This is my about page</p>
