08319b72aa01e72e92d47752380fec8f29fc354d
ericyork <eyork@clarkson.edu>
2022-12-16 15:13:22 -0500

    tests adding a blog post

--- src/routes/blog/data.js
+++ src/routes/blog/data.js
@@ -1,7 +1,7 @@
 export const posts = [
 	{
 		slug: 'welcome',
-		title: 'Welcome to the Aperture Science computer-aided enrichment center',
+		title: 'Welcome to Aperture Science',
 		content:
 			'<p>We hope your brief detention in the relaxation vault has been a pleasant one.</p><p>Your specimen has been processed and we are now ready to begin the test proper.</p>'
 	},
@@ -17,5 +17,10 @@ export const posts = [
 		slug: 'cake',
 		title: 'This was a triumph',
 		content: "<p>I'm making a note here: HUGE SUCCESS.</p>"
+	},
+	{
+		slug: 'frantic',
+		title: "It's all working now",
+		content: "<p>I guess I should have known that was going to be an issue.</p>"
 	}
 ];
