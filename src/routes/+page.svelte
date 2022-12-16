22c6ad37d9fe3e2ed3d4a0f61f91c5232e658956
ericyork <eyork@clarkson.edu>
2022-12-16 11:07:12 -0500

    adds nav and about route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<nav>
+	<a href="/">home</a>
+	<a href="/about">about</a>
+</nav>
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Working through the new <a href="https://learn.svelte.dev/tutorial">sveltekit tutorial</a>.</p>
